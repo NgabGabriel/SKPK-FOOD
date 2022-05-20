abcdbdf2bcf663bfe4edc0cb8281e9c3af7bbd3a
NgabGabriel <105896632+NgabGabriel@users.noreply.github.com>
2022-05-20 12:28:08 +0700

    Upload

---
+++ database.js
@@ -0,0 +1,107 @@
+let makanan = [];
+
+let makanan_detail = {
+    foto: "https://www.jakartanotebook.com/blog/content/images/2020/01/jus-cover.jpg",
+    harga: 15000,
+    kode: "BRG001",
+    nama: "Jus",
+    rating: "5.0",
+    stock: 10
+
+};
+makanan.push(makanan_detail);
+
+makanan_detail = {
+    foto: "https://www.masakapahariini.com/wp-content/uploads/2019/11/resep-mie-bakso.jpg",
+    harga: 15000,
+    kode:"BRG002",
+    nama: "Bakso",
+    rating: "4,5",
+    stock: "20",
+
+};
+makanan.push(makanan_detail);
+
+makanan_detail = {
+    foto: "http://assets.kompasiana.com/items/album/2020/07/30/whatsapp-image-2020-07-26-at-15-09-13-5f227332d541df2648175cc2.jpeg",
+    harga: 500,
+    nama: "Heci",
+    rating: "5.0",
+    stock: "100",
+
+};
+makanan.push(makanan_detail);
+
+makanan_detail = {
+    foto: "https://thumb.viva.co.id/media/frontend/thumbs3/2015/04/02/305500_ilustrasi-permen_665_374.jpg",
+    harga: 100,
+    kode:"BRG004",
+    nama:"Permen",
+    rating: "5.0",
+    stock: "100",
+};
+makanan.push(makanan_detail);
+
+makanan_detail = {
+    foto: "https://biaya.info/wp-content/uploads/2021/04/Air_Mineral2.jpeg",
+    harga: 3000,
+    kode:"BRG005",
+    nama: "Air Mineral",
+    rating: "5.0",
+    stock: "50",
+};
+makanan.push(makanan_detail);
+
+makanan_detail = {
+    foto: "http://kbu-cdn.com/dk/wp-content/uploads/nasi-goreng-setan.jpg",
+    harga: 10000,
+    kode: "BRG006",
+    nama: "Nasi Goreng",
+    rating: "5.0",
+    stock: "100",
+};
+makanan.push(makanan_detail);
+ 
+makanan_detail = {
+    foto: "https://www.resepistimewa.com/wp-content/uploads/tahu-telur.jpg",
+    harga: 12000,
+    kode: "BRG007",
+    nama: "Tahu Telor",
+    rating: "5.0",
+    stock: "100",
+};
+makanan.push(makanan_detail);
+
+makanan_detail = {
+    foto: "https://upload.wikimedia.org/wikipedia/commons/4/48/Rujak_Buah_%28Indonesian_Fruit_Salad%29.JPG",
+    harga: 10000,
+    kode: "BRG008",
+    nama: "Rujak",
+    rating: "5.0",
+    stock: "100",
+};
+makanan.push(makanan_detail);
+
+makanan_detail = {
+    foto: "http://topwisata.info/wp-content/uploads/2021/06/Roti2BBakar2BBandung2B252812529.jpg",
+    harga: 15000,
+    kode: "BRG009",
+    nama: "Roti Bakar",
+    rating: "5.0",
+    stock: "100",
+};
+makanan.push(makanan_detail);
+
+makanan_detail = {
+    foto: "https://cdn1-production-images-kly.akamaized.net/sRmE_LFohL1ZELlgJrSMKTtCigM=/673x379/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/2485920/original/013437000_1543329222-resep-mi-ayam-pangsit-spesial-rasa-mantap-buatan-sendiri.jpg",
+    harga: 8000,
+    kode: "BRG010",
+    nama: "Mie Pangsit",
+    rating: "5.0",
+    stock: "100",
+};
+
+makanan.push(makanan_detail);
+console.log(makanan)
+
+
